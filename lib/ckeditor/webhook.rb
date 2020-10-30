487c41b3d46a63a89d8277465b094026d6a24121
Jack Clayton <clayjs0@gmail.com>
2020-10-30 09:34:51 -0600

    Initial bundle gem

---
+++ lib/ckeditor/webhook.rb
@@ -0,0 +1,8 @@
+require "ckeditor/webhook/version"
+
+module Ckeditor
+  module Webhook
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/ckeditor/webhook_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ckeditor::Webhook do
+  it "has a version number" do
+    expect(Ckeditor::Webhook::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
