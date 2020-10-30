2406bba44e16042a0d09368dad7af560db946148
Jack Clayton <clayjs0@gmail.com>
2020-10-30 11:47:29 -0600

    Add event

---
+++ lib/ckeditor/webhook/event.rb
@@ -0,0 +1,39 @@
+require "time"
+
+# A generic webhook notification from Ckeditor Cloud Services.
+#
+# @see  https://ckeditor.com/docs/cs/latest/guides/webhooks/overview.html
+module Ckeditor
+  module Webhook
+    class InvalidPayload < StandardError; end
+
+    class Event
+      PROPERTIES = %i[event environment_id payload sent_at]
+
+      # @raise  [InvalidPayload]  raised if required properties are missing
+      def initialize(payload)
+        raise InvalidPayload.new(
+          "Expected a hash with :#{PROPERTIES.join(", :")} keys"
+        ) unless PROPERTIES.all? { |s| payload.key? s }
+
+        @payload = payload
+      end
+
+      def environment_id
+        @payload[:environment_id]
+      end
+
+      def payload
+        @payload[:payload]
+      end
+
+      def sent_at
+        ::Time.parse @payload[:sent_at]
+      end
+
+      def type
+        @payload[:event]
+      end
+    end
+  end
+end
---
+++ spec/ckeditor/webhook/event_spec.rb
@@ -0,0 +1,99 @@
+module Ckeditor
+  module Webhook
+    RSpec.describe Event do
+      # @see  https://ckeditor.com/docs/cs/latest/guides/webhooks/overview.html#webhook-format
+      let(:payload) do
+        {
+          event:          "foo",
+          environment_id: "bar",
+          payload:        { baz: "qux" },
+          sent_at:        Time.now.utc.strftime("%Y-%m-%dT%H:%M:%S.%3NZ")
+        }
+      end
+
+      describe "#initialize" do
+        context "when :event key is missing" do
+          before { payload.delete :event }
+
+          it "raises exception" do
+            expect {
+              Event.new(payload)
+            }.to raise_error(InvalidPayload)
+          end
+        end
+
+        context "when :environment_id key is missing" do
+          before { payload.delete :environment_id }
+
+          it "raises exception" do
+            expect {
+              Event.new(payload)
+            }.to raise_error(InvalidPayload)
+          end
+        end
+
+        context "when :payload key is missing" do
+          before { payload.delete :payload }
+
+          it "raises exception" do
+            expect {
+              Event.new(payload)
+            }.to raise_error(InvalidPayload)
+          end
+        end
+
+        context "when :sent_at key is missing" do
+          before { payload.delete :sent_at }
+
+          it "raises exception" do
+            expect {
+              Event.new(payload)
+            }.to raise_error(InvalidPayload)
+          end
+        end
+
+        context "when required keys are present" do
+          it "returns event" do
+            expect(Event.new(payload)).to be_instance_of(Event)
+          end
+        end
+      end
+
+      describe "#environment_id" do
+        let(:event) { Event.new(payload) }
+
+        it "returns environment_id" do
+          expect(event.environment_id).to eq(payload[:environment_id])
+        end
+      end
+
+      describe "#payload" do
+        let(:event) { Event.new(payload) }
+
+        it "returns payload" do
+          expect(event.payload).to eq(payload[:payload])
+        end
+      end
+
+      describe "#sent_at" do
+        let(:event) { Event.new(payload) }
+
+        it "returns a Time" do
+          expect(event.sent_at).to be_instance_of(::Time)
+        end
+
+        it "returns parsed time" do
+          expect(event.sent_at).to be_within(1).of Time.now
+        end
+      end
+
+      describe "#type" do
+        let(:event) { Event.new(payload) }
+
+        it "returns type" do
+          expect(event.type).to eq(payload[:event])
+        end
+      end
+    end
+  end
+end
