0565824d9aecaf96827da9443372ed7a1c06b0c9
Jack Clayton <clayjs0@gmail.com>
2020-10-30 11:47:43 -0600

    Add construct_event method

--- lib/ckeditor/webhook.rb
+++ lib/ckeditor/webhook.rb
@@ -1,8 +1,59 @@
 require "ckeditor/webhook/version"
 
+require "ckeditor/webhook/event"
+
+require "json"
+require "openssl"
+require "uri"
+
 module Ckeditor
   module Webhook
     class Error < StandardError; end
-    # Your code goes here...
+    class SignatureVerificationError < StandardError; end
+
+    class << self
+      # Returns an Event if the webhook signature is valid.
+      #
+      # @param  secret     [String]   the CKEditor Cloud Services API secret
+      # @param  payload    [Hash]     the webhook's payload as a hash
+      # @param  signature  [String]   the request's `X-CS-Signature` header
+      # @param  timestamp  [Integer]  the request's `X-CS-Timestamp` header
+      # @param  method     [String]   the request's method (defaults to "POST")
+      # @param  url        [String]   the request's url
+      #
+      # @raise  [SignatureVerificationError]  if signature's don't match
+      #
+      # @return  [Event]
+      #
+      def construct_event(secret:, payload:, signature:, timestamp:, url:, method: "POST")
+        event = message(method: method, url: url, timestamp: timestamp, payload: payload)
+
+        raise SignatureVerificationError if signature != message_signature(message: event, secret: secret)
+
+        Event.new(payload)
+      end
+
+      private
+
+      # Returns the event's message for signing following CKEditor's rules.
+      #
+      # @see  https://ckeditor.com/docs/cs/latest/examples/security/request-signature-nodejs.html
+      def message(method:, url:, timestamp:, payload:)
+        uri    = URI.parse(url)
+        path   = "#{uri.path}?#{uri.query}"
+        method = method.upcase
+        body   = payload.to_json
+
+        "#{method}#{path}#{timestamp}#{body}"
+      end
+
+      def message_signature(message:, secret:)
+        ::OpenSSL::HMAC.hexdigest(
+          "SHA256",
+          secret,
+          message
+        )
+      end
+    end
   end
 end
--- spec/ckeditor/webhook_spec.rb
+++ spec/ckeditor/webhook_spec.rb
@@ -3,7 +3,50 @@ RSpec.describe Ckeditor::Webhook do
     expect(Ckeditor::Webhook::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  # The success parameters and signature are taken from CKEdior's example
+  # algorithm.
+  #
+  # @see  https://ckeditor.com/docs/cs/latest/examples/security/request-signature-nodejs.html
+  describe "::construct_event" do
+    let(:secret)    { "SECRET" }
+    let(:method)    { "POST" }
+    let(:url)       { "http://demo.example.com/webhook?a=1" }
+    let(:timestamp) { 1563276169752 }
+    let(:payload)   { { a: 1 } }
+
+    context "when signatures do not match" do
+      let(:signature) { "WRONG" }
+
+      it "raises SignatureVerificationError exception" do
+        expect {
+          Ckeditor::Webhook.construct_event(
+            url:       url,
+            secret:    secret,
+            method:    method,
+            signature: signature,
+            timestamp: timestamp,
+            payload:   payload
+          )
+        }.to raise_error(Ckeditor::Webhook::SignatureVerificationError)
+      end
+    end
+
+    context "when signatures do match" do
+      let(:signature) { "56ac656c7f932c5b775be28949e90af9a2356eae2826539f10ab6526a0eec762" }
+
+      it "returns event" do
+        # Actually calling Event.new with payload will raise PayloadInvalid exception.
+        expect(Ckeditor::Webhook::Event).to receive(:new).with(payload)
+
+        Ckeditor::Webhook.construct_event(
+          url:       url,
+          secret:    secret,
+          method:    method,
+          signature: signature,
+          timestamp: timestamp,
+          payload:   payload
+        )
+      end
+    end
   end
 end
